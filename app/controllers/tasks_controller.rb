c060bf94e74363d53ab43db01fc9641a365688ac
paul lahana <paul.lahana@gmail.com>
2019-11-12 14:29:44 +0100

    taks complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %I[show edit update destroy]
   def index
     @tasks = Task.all
   end
@@ -27,7 +28,17 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
